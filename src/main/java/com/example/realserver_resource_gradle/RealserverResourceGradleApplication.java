ac0ab48dec9ee26f0ae339736ce067a0814d665a
wpdusdlrj97 <48294315+wpdusdlrj97@users.noreply.github.com>
2019-11-19 17:45:19 +0900

    Initial commit

---
+++ src/main/java/com/example/realserver_resource_gradle/RealserverResourceGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.realserver_resource_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealserverResourceGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealserverResourceGradleApplication.class, args);
+	}
+
+}
